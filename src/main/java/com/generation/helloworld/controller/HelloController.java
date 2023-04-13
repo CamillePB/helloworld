1642b3f156a2315e230aeb7519e6ef2ae5910dc4
CamillePB <camille.pedro.bueno@hotmail.com>
2023-04-12 22:54:17 -0300

    12/04/2023 up

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -7,33 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-	
+
 	@GetMapping
 	public String hello() {
 		return "<b>Hello Turma 62!</b>";
 	}
-	
+
 	@GetMapping("/bsm")
 	public String bsm() {
-		return "<b>BSM's</b>"+
-	"<br>Mentalidade de Crescimento"+
-				"<br>Persistência"+
-	"<br>Responsabilidade Pessoal"+
-				"<br>Orientação ao Futuro"+
-	"<br>Comunicação"+
-				"<br>Orientação ao Detalhe"+
-	"<br>Proatividade"+
-	"<br>Trabalho em Equipe"+
-	"<br>Dar e Receber"+
-	"<br>Gestão do Tempo<br>";
-		
+		return "<b>BSM's</b>" + "<br>Mentalidade de Crescimento" + "<br>Persistência" + "<br>Responsabilidade Pessoal"
+				+ "<br>Orientação ao Futuro" + "<br>Comunicação" + "<br>Orientação ao Detalhe" + "<br>Proatividade"
+				+ "<br>Trabalho em Equipe" + "<br>Dar e Receber" + "<br>Gestão do Tempo<br>";
+
 	}
-	
+
 	@GetMapping("/objetivo")
 	public String objetivo() {
-		return "<b>Objetivo da semana:</b>"+
-				"<br>Proatividade"+
-				"<br>Orientação aos detalhes";
+		return "<b>Objetivo da semana:</b>" + "<br>Proatividade" + "<br>Orientação aos detalhes";
 	}
 
 }
